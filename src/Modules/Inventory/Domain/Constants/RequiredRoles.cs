58d3d5f0e1b1fb9fbe1ca0eaf650181831bbaa17
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2026-07-31 21:31:16 -0500

    RequiredRoles Added to use in endpoints

---
+++ src/Modules/Inventory/Domain/Constants/RequiredRoles.cs
@@ -0,0 +1,8 @@
+namespace PIMS_MS.Modules.Inventory.Domain.Constants;
+
+public class RequiredRoles
+{
+    public const string ConsultantLogistic = nameof(ConsultantLogistic);
+    public const string OperatorManager = nameof(OperatorManager);
+    public const string Guest = nameof(Guest);
+}
