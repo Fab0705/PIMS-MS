38521c7997ce9f90e0984f4a54b60ea2e8f8b31c
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2026-07-31 22:07:10 -0500

    RequiredRoles added

---
+++ src/Modules/Logistics/Domain/Constants/RequiredRoles.cs
@@ -0,0 +1,7 @@
+namespace PIMS_MS.Modules.Logistics.Domain.Constants;
+
+public class RequiredRoles
+{
+    public const string ConsultantLogistic = nameof(ConsultantLogistic);
+    public const string OperatorManager = nameof(OperatorManager);
+}
