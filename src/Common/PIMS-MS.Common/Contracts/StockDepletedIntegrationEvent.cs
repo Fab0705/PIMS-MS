afb7cfb7fd8304234a14ecefcb940f8c19993d12
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2026-08-13 11:51:02 -0500

    add: Record Integration event for Stock Depleted

---
+++ src/Common/PIMS-MS.Common/Contracts/StockDepletedIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PIMS_MS.Common.Contracts;
+
+public record StockDepletedIntegrationEvent(
+    Guid SparePartId,
+    Guid LocationId,
+    int CurrentQuantity
+) : INotification;
