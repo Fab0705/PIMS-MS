27c53a41d5d48a4f0d38bd0c4028ccc4286ab8ab
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2026-08-13 11:52:27 -0500

    add: Event Handler Notification for Stock Depleted

---
+++ src/Modules/Inventory/Features/StockManagement/EventHandlers/StockDepletedHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using PIMS_MS.Common.Contracts;
+using PIMS_MS.Modules.Inventory.Domain.Events;
+
+namespace PIMS_MS.Modules.Inventory.Features.StockManagement.EventHandlers;
+public class StockDepletedHandler : INotificationHandler<StockDepletedEvent>
+{
+    private readonly IPublisher _publisher;
+    public StockDepletedHandler(IPublisher publisher)
+    {
+        _publisher = publisher;
+    }
+
+    public async Task Handle(StockDepletedEvent notification, CancellationToken cancellationToken)
+    {
+        var integrationEvent = new StockDepletedIntegrationEvent(
+            notification.SparePartId,
+            notification.LocationId,
+            notification.CurrentQuantity
+        );
+
+        await _publisher.Publish(integrationEvent, cancellationToken);
+    }
+}
